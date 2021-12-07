eef35824c8f1b1deed27beb118dd5835e837eb0b
carlospuigserver <cpuiggut@myuax.com>
2021-12-07 14:04:24 +0100

    dibujar laberinto

--- dibujo.py
+++ dibujo.py
@@ -10,13 +10,14 @@ ventana=turtle.setup(700,700)
 
 # Creamos una clase que nos permita dibujar los cuadros en la pantLLa, la cual será una subclase del módulo turtle, y la inicializamos
 
-class lab(turtle.Turtle):
+class pen(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)  #Después de inicializarla, le damos propiedades
         self.shape("square")
         self.color("white")
         self.penup()  #para que no deje rastro
         self.speed(0) #para que se pueda ver como se va dibujamdo
+    
 
 niveles=[]
 #definir nivel
@@ -25,6 +26,14 @@ nivel_1=[
     "XXXXXXXXXXXXXXXXXXXXXXXXX",
     "XXXXXXXXXXXXXXXXXXXXXXXXX",
     "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XXXXXXXXXX       XXXXXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XXXX             XXXXXXXX",
     "XXXXXXXXXXXXXXXXXXXXXXXXX",
     "XXXXXXXXXXXXXXXXXXXXXXXXX",
     "XXXXXXXXXXXXXXXXXXXXXXXXX",
@@ -37,13 +46,24 @@ nivel_1=[
     "XXXXXXXXXXXXXXXXXXXXXXXXX",
     "XXXXXXXXXXXXXXXXXXXXXXXXX",
     "XXXXXXXXXXXXXXXXXXXXXXXXX",
-    "XXXXXXXXXXXXXXXXXXXXXXXXX",
-    "XXXXXXXXXXXXXXXXXXXXXXXXX",
-    "XXXXXXXXXXXXXXXXXXXXXXXXX",
-    "XXXXXXXXXXXXXXXXXXXXXXXXX",
-    "XXXXXXXXXXXXXXXXXXXXXXXXX",
-    "XXXXXXXXXXXXXXXXXXXXXXXXX",
-    "XXXXXXXXXXXXXXXXXXXXXXXXX",
-    "XXXXXXXXXXXXXXXXXXXXXXXXX",
-    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    
 ]
+niveles.append(nivel_1)
+
+#definir función para empezar a dibujar el laberinto
+def iniciar_lab(nivel):
+    for fila in range(len(nivel)):    
+            for  column in range(len(nivel[fila])):   #Lo que estoy haciendo ahora es iterar cada uno de los elementos de la lista por filas y por columnas, y donde haya una "X", sustituirlo por un cuadrado
+                letra_x=nivel[fila][column]
+                screen_x=-288 + (column*24)
+                screen_y=+288 - (fila*24)
+
+                if letra_x=="X":
+                    pen.goto(screen_x,screen_y)
+                    pen.stamp()
+                
+                    
+
+pen = pen()
+                                                    
+iniciar_lab(niveles[0])
